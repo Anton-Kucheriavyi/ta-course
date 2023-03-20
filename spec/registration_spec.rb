3d702d3aedccd3c4301808742dcb2bdc168effa3
Anton Kucheriavyi <a956339793@gmail.com>
2023-03-20 11:28:16 +0200

    Added using page object for registration page

--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -1,44 +1,42 @@
 feature 'User registration', js: true do
-  scenario 'User is able to register' do
-    visit('https://gitlab.testautomate.me/')
-
-    find(:dqs, 'register_link').click
-
-    expect(page).to have_content 'First name'
-  end
-
   scenario 'User can register to the system' do
-    visit('https://gitlab.testautomate.me/')
+    @register_page = RegistrationPage.new
+    @register_page.load
 
-    find(:dqs, 'register_link').click
-    find('#new_user_first_name').set 'John'
-    find('#new_user_last_name').set 'Doe'
-    find('#new_user_username').set Faker::Name.unique.first_name
+    @register_page.first_name.set 'John'
+    @register_page.last_name.set 'Doe'
+    @register_page.user_name.set Faker::Name.unique.first_name
+
     expect(page).to have_selector('.validation-success.field-validation')
 
-    find('#new_user_email').set Faker::Internet.email
-    find('#new_user_password').set 'test123456!'
-    find(:dqs, 'new_user_register_button').click
-    select('Software Developer', from: 'Role')
-    select('A different reason', from: "I'm signing up for GitLab because:")
+    @register_page.email.set Faker::Internet.email
+    @register_page.password.set 'test123456!'
+    @register_page.registration_button.click
 
-    find('#jobs_to_be_done_other').set 'Whatever reason' if find('#jobs_to_be_done_other', visible: false).visible?
+    @register_page.roles.first.select('Software Developer')
+    @register_page.objectives.first.select("A different reason")
 
-    find(:dqs, 'get_started_button').click
+    @register_page.wait_until_other_objective_visible(wait: 1)
+
+    @register_page.other_objective.set 'Whatever reason'
+    @register_page.finish_registration_button.click
 
     expect(page).to have_content 'Welcome to GitLab'
   end
 
   scenario 'User can not register to the system with not valid password' do
-    visit('https://gitlab.testautomate.me/')
+    @register_page = RegistrationPage.new
+    @register_page.load
 
-    find(:dqs, 'register_link').click
-    find('#new_user_first_name').set 'John'
-    find('#new_user_last_name').set 'Doe'
-    find('#new_user_username').set Faker::Name.unique.first_name
-    find('#new_user_email').set Faker::Internet.email
-    find('#new_user_password').set 'test'
-    find(:dqs, 'new_user_register_button').click
+    @register_page.first_name.set 'John'
+    @register_page.last_name.set 'Doe'
+    @register_page.user_name.set Faker::Name.unique.first_name
+
+    expect(page).to have_selector('.validation-success.field-validation')
+
+    @register_page.email.set Faker::Internet.email
+    @register_page.password.set 'test'
+    @register_page.registration_button.click
 
     expect(page).to have_content 'Minimum length is 8 characters'
   end

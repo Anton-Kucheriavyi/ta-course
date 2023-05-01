ecded8ff2a77dfcbb5799743c490fda06e1bc9ec
Anton Kucheriavyi <a956339793@gmail.com>
2023-05-01 15:06:22 +0300

    Add using helper methods, models, env variables

---
+++ modules/helpers/helper.rb
@@ -0,0 +1,33 @@
+module Helpers
+  def login_user(login, password)
+    @login_page = LoginPage.new
+    @login_page.load
+
+    @login_page.login_field.set login
+    @login_page.password_field.set password
+    @login_page.submit_button.click
+  end
+
+  def register_user(user)
+    @register_page = RegistrationPage.new
+    @register_page.load
+
+    @register_page.first_name.set user.firstname
+    @register_page.last_name.set user.lastname
+    @register_page.user_name.set user.username
+
+    expect(page).to have_selector('.validation-success.field-validation')
+
+    @register_page.email.set user.email
+    @register_page.password.set 'test123456!'
+    @register_page.registration_button.click
+
+    @register_page.role.select('Software Developer')
+    @register_page.objective.select("A different reason")
+
+    @register_page.wait_until_other_objective_visible(wait: 1)
+
+    @register_page.other_objective.set 'Whatever reason'
+    @register_page.finish_registration_button.click
+  end
+end
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -1,27 +1,15 @@
 feature 'User registration', js: true do
   scenario 'User can register to the system' do
-    @register_page = RegistrationPage.new
-    @register_page.load
-
-    @register_page.first_name.set 'John'
-    @register_page.last_name.set 'Doe'
-    @register_page.user_name.set Faker::Name.unique.first_name
-
-    expect(page).to have_selector('.validation-success.field-validation')
-
-    @register_page.email.set Faker::Internet.email
-    @register_page.password.set 'test123456!'
-    @register_page.registration_button.click
-
-    @register_page.role.select('Software Developer')
-    @register_page.objective.select("A different reason")
-
-    @register_page.wait_until_other_objective_visible(wait: 1)
-
-    @register_page.other_objective.set 'Whatever reason'
-    @register_page.finish_registration_button.click
+    user = User.new
+    register_user user
 
     expect(page).to have_content 'Welcome to GitLab'
+
+    @home_page = HomePage.new
+    @home_page.menu.user_picture.click
+
+    expect(@home_page.menu.user_name).to have_content user.firstname
+    expect(@home_page.menu.user_name).to have_content user.lastname
   end
 
   scenario 'User can not register to the system with not valid password' do
@@ -30,11 +18,11 @@ feature 'User registration', js: true do
 
     @register_page.first_name.set 'John'
     @register_page.last_name.set 'Doe'
-    @register_page.user_name.set Faker::Name.unique.first_name
+    @register_page.user_name.set FFaker::Name.unique.first_name
 
     expect(page).to have_selector('.validation-success.field-validation')
 
-    @register_page.email.set Faker::Internet.email
+    @register_page.email.set FFaker::Internet.email
     @register_page.password.set 'test'
     @register_page.registration_button.click
 
